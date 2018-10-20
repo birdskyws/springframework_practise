2ffe7d7e3a9cb46ab389d7f0b176d24b33f920ea
birdskyws <birdskyws@163.com>
2018-10-20 09:41:24 +0800

    获取资源文件，ApplicationContextAware接口应用

---
+++ src/main/java/org/birdsky/ioc/resource/MoocResource.java
@@ -0,0 +1,23 @@
+package org.birdsky.ioc.resource;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+
+
+public class MoocResource implements ApplicationContextAware {
+    private ApplicationContext applicationContext;
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+    public void readResource() throws IOException {
+        Resource resource = applicationContext.getResource("classpath:config.txt");
+        System.out.println(resource.getFilename());
+        System.out.println(resource.contentLength());
+
+    }
+}
